409a0599ce7e498b9755c4426a2ceb210e0d8ee2
Hamidulla Kazimov <hihamidkz@inbox.ru>
2019-04-25 22:59:00 +0700

    Добавлены аннотации сущности в класс модели

--- src/main/java/ru/eltex/vkbot/model/Post.java
+++ src/main/java/ru/eltex/vkbot/model/Post.java
@@ -1,13 +1,28 @@
 package ru.eltex.vkbot.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "posts")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Post implements Serializable {
 
+    @Id
+    @GeneratedValue
+    private int id;
     private String text;
+
+    @Column(name = "user_id")
     private int userId;
     private int date;
+
+    @Transient
     private transient boolean removePost;
+
+    public Post() {
+
+    }
 
     public Post(String text, int userId, int date) {
         this.text = text;
